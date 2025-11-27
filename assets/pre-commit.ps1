d4aa1a0f2e1ca6a714c225e6f90b54edfcec31ac
Fenhl <fenhl@fenhl.net>
2025-11-27 05:26:41 +0000

    Remove redundant branch param from deps

--- assets/pre-commit.ps1
+++ assets/pre-commit.ps1
@@ -7,13 +7,13 @@ if (-not $?)
 }
 
 # copy the tree to the WSL file system to improve compile times
-wsl rsync --delete -av /mnt/c/Users/fenhl/git/github.com/fenhl/ctrlflow/stage/ /home/fenhl/wslgit/github.com/fenhl/ctrlflow/ --exclude target
+wsl -d ubuntu-m2 rsync --mkpath --delete -av /mnt/c/Users/fenhl/git/github.com/fenhl/ctrlflow/stage/ /home/fenhl/wslgit/github.com/fenhl/ctrlflow/ --exclude target
 if (-not $?)
 {
     throw 'Native Failure'
 }
 
-wsl env -C /home/fenhl/wslgit/github.com/fenhl/ctrlflow cargo check --all-features
+wsl -d ubuntu-m2 env -C /home/fenhl/wslgit/github.com/fenhl/ctrlflow /home/fenhl/.cargo/bin/cargo check --all-features
 if (-not $?)
 {
     throw 'Native Failure'
